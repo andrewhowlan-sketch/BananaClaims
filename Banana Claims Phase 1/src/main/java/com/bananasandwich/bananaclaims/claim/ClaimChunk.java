15297999e05db46002ab5d379aba5a35f2d735b3
andrewhowlan-sketch <andrewhowlan@gmail.com>
2026-07-06 19:02:41 -0700

    Delete Banana Claims Phase 1 directory

--- Banana Claims Phase 1/src/main/java/com/bananasandwich/bananaclaims/claim/ClaimChunk.java
+++
@@ -1,7 +0,0 @@
-package com.bananasandwich.bananaclaims.claim;
-
-public record ClaimChunk(String dimension, int x, int z) {
-    public String key() {
-        return dimension + ":" + x + ":" + z;
-    }
-}
